ea6ee66e52427a65576e3effbdee42835ed3a7aa
VlaVer98 <verholantsev1998@yandex.ru>
2020-12-25 11:08:08 +0700

    Класс для получения данных с сервера

---
+++ ConsoleApp/Models/Data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp.Models
+{
+    internal class Data
+    {
+        public string GetAsString(string url)
+        {
+            StringBuilder response = new StringBuilder();
+            WebClient client = new WebClient();
+
+            using (Stream stream = client.OpenRead(url))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        response.Append(line);
+                    }
+                }
+            }
+
+            return response.ToString();
+        }
+    }
+}
