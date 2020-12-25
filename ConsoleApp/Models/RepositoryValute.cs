e2703789f31079b720ddbd2cf33a862c0407c421
VlaVer98 <verholantsev1998@yandex.ru>
2020-12-25 13:03:26 +0700

    Класс схема для десерилиализации Json

---
+++ ConsoleApp/Models/RepositoryValute.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ConsoleApp.Models
+{
+    internal class RepositoryValute
+    {
+        [JsonProperty("valute")]
+        public readonly Dictionary<string, Valute> valutes = new Dictionary<string, Valute>();
+
+        internal class Valute
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public decimal Nominal { get; set; }
+            public string Name { get; set; }
+            public decimal Value { get; set; }
+            public decimal Previous { get; set; }
+        }
+    }
+}
