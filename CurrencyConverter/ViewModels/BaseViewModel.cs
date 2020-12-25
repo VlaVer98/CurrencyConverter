8486f27abd644e5cdb00001608596f0671129734
VlaVer98 <verholantsev1998@yandex.ru>
2020-12-25 20:03:59 +0700

    Создание ViewModel для MainPage.

---
+++ CurrencyConverter/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace CurrencyConverter.ViewModels
+{
+    abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ CurrencyConverter/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace CurrencyConverter.ViewModels
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        string _Title = "Загрузка данных";
+        public string Title {
+            get => _Title;
+            set
+            {
+                _Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        private Visibility _ViewDownload = Visibility.Visible;
+        public Visibility ViewDownload { 
+            get => _ViewDownload; 
+            set {
+                _ViewDownload = value;
+                OnPropertyChanged("ViewDownload");
+            } 
+        }
+
+        private Visibility _ViewMain = Visibility.Collapsed;
+        public Visibility ViewMain { 
+            get => _ViewMain; 
+            set
+            {
+                _ViewMain = value;
+                OnPropertyChanged("ViewMain");
+            }
+        }
+
+        private Visibility _ViewSelect = Visibility.Collapsed;
+        public Visibility ViewSelect { 
+            get => _ViewSelect;
+            set
+            {
+                _ViewSelect = value;
+                OnPropertyChanged("ViewSelect");
+            } 
+        }
+    }
+}
