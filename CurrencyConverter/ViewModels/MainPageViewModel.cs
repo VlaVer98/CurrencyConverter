6c0c8ef7cb1f965c1833fc7e8819aac9d578dfc4
VlaVer98 <verholantsev1998@yandex.ru>
2020-12-26 14:22:02 +0700

    Реализация команды по загрузки данных из сети и пивязка к виду

--- CurrencyConverter/ViewModels/MainPageViewModel.cs
+++ CurrencyConverter/ViewModels/MainPageViewModel.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using CurrencyConverter.Commands;
+using CurrencyConverter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Windows.UI.Xaml;
+using static CurrencyConverter.Models.RepositoryValute;
 
 namespace CurrencyConverter.ViewModels
 {
     class MainPageViewModel : BaseViewModel
     {
+        Dictionary<string, Valute> valutes;
+        Converter converter;
+
         string _Title = "Загрузка данных";
         public string Title {
             get => _Title;
@@ -23,8 +31,14 @@ namespace CurrencyConverter.ViewModels
         public Visibility ViewDownload { 
             get => _ViewDownload; 
             set {
+                _Title = "Загузка данных";
+                _ViewMain = Visibility.Collapsed;
+                _ViewSelect = Visibility.Collapsed;
                 _ViewDownload = value;
+                OnPropertyChanged("Title");
                 OnPropertyChanged("ViewDownload");
+                OnPropertyChanged("ViewMain");
+                OnPropertyChanged("ViewSelect");
             } 
         }
 
@@ -33,8 +47,14 @@ namespace CurrencyConverter.ViewModels
             get => _ViewMain; 
             set
             {
+                _Title = "Конвертер валют";
+                _ViewDownload = Visibility.Collapsed;
+                _ViewSelect = Visibility.Collapsed;
                 _ViewMain = value;
+                OnPropertyChanged("Title");
+                OnPropertyChanged("ViewDownload");
                 OnPropertyChanged("ViewMain");
+                OnPropertyChanged("ViewSelect");
             }
         }
 
@@ -43,9 +63,45 @@ namespace CurrencyConverter.ViewModels
             get => _ViewSelect;
             set
             {
+                _Title = "Выбор валют";
+                _ViewDownload = Visibility.Collapsed;
+                _ViewMain = Visibility.Collapsed;
                 _ViewSelect = value;
+                OnPropertyChanged("Title");
+                OnPropertyChanged("ViewDownload");
+                OnPropertyChanged("ViewMain");
                 OnPropertyChanged("ViewSelect");
             } 
+        }
+        public ICommand LoadedCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            LoadedCommand = new RelayCommand(Loaded);
+        }
+        private async void Loaded()
+        {
+            bool operation = true;
+
+            await Task.Run(() => {
+                try
+                {
+                    valutes = new RepositoryValute(new ValutesBuilder()).GetValutes();
+                    converter = new Converter(valutes["USD"], valutes["EUR"]);
+                }
+                catch (Exception) {
+                    operation = false;
+                }
+            });
+
+            if (operation)
+            {
+                ViewMain = Visibility.Visible;
+            }
+            else
+            {
+                //TODO вид ошибки
+            }
         }
     }
 }
