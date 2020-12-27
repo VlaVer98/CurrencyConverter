94c019f0efc6b4b53d24c6187b344206b8fdd632
VlaVer98 <verholantsev1998@yandex.ru>
2020-12-27 14:48:58 +0700

    Связывание списка валют с видом. Реализация логики выбора валюты

--- CurrencyConverter/ViewModels/MainPageViewModel.cs
+++ CurrencyConverter/ViewModels/MainPageViewModel.cs
@@ -2,6 +2,7 @@
 using CurrencyConverter.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,8 +15,7 @@ namespace CurrencyConverter.ViewModels
 {
     class MainPageViewModel : BaseViewModel
     {
-        Dictionary<string, Valute> valutes;
-        Converter converter;
+        private Converter _Converter;
 
         #region Views
         private Visibility _ViewDownload = Visibility.Visible;
@@ -26,12 +26,15 @@ namespace CurrencyConverter.ViewModels
             {
                 _Title = "Загузка данных";
                 _ViewMain = Visibility.Collapsed;
-                _ViewSelect = Visibility.Collapsed;
+                _ViewSelect1 = Visibility.Collapsed;
+                _ViewSelect2 = Visibility.Collapsed;
                 _ViewDownload = value;
                 OnPropertyChanged("Title");
                 OnPropertyChanged("ViewDownload");
                 OnPropertyChanged("ViewMain");
                 OnPropertyChanged("ViewSelect");
+                OnPropertyChanged("ViewSelect1");
+                OnPropertyChanged("ViewSelect2");
             }
         }
 
@@ -43,33 +46,57 @@ namespace CurrencyConverter.ViewModels
             {
                 _Title = "Конвертер валют";
                 _ViewDownload = Visibility.Collapsed;
-                _ViewSelect = Visibility.Collapsed;
+                _ViewSelect1 = Visibility.Collapsed;
+                _ViewSelect2 = Visibility.Collapsed;
                 _ViewMain = value;
                 OnPropertyChanged("Title");
                 OnPropertyChanged("ViewDownload");
                 OnPropertyChanged("ViewMain");
-                OnPropertyChanged("ViewSelect");
+                OnPropertyChanged("ViewSelect1");
+                OnPropertyChanged("ViewSelect2");
             }
         }
 
-        private Visibility _ViewSelect = Visibility.Collapsed;
-        public Visibility ViewSelect
+        private Visibility _ViewSelect1 = Visibility.Collapsed;
+        public Visibility ViewSelect1
         {
-            get => _ViewSelect;
+            get => _ViewSelect1;
             set
             {
                 _Title = "Выбор валют";
                 _ViewDownload = Visibility.Collapsed;
                 _ViewMain = Visibility.Collapsed;
-                _ViewSelect = value;
+                _ViewSelect1 = value;
+                _ViewSelect2 = Visibility.Collapsed;
+                OnPropertyChanged("Title");
+                OnPropertyChanged("ViewDownload");
+                OnPropertyChanged("ViewMain");
+                OnPropertyChanged("ViewSelect1");
+                OnPropertyChanged("ViewSelect2");
+            }
+        }
+
+        private Visibility _ViewSelect2 = Visibility.Collapsed;
+        public Visibility ViewSelect2
+        {
+            get => _ViewSelect2;
+            set
+            {
+                _Title = "Выбор валют";
+                _ViewDownload = Visibility.Collapsed;
+                _ViewMain = Visibility.Collapsed;
+                _ViewSelect1 = Visibility.Collapsed;
+                _ViewSelect2 = value;
                 OnPropertyChanged("Title");
                 OnPropertyChanged("ViewDownload");
                 OnPropertyChanged("ViewMain");
                 OnPropertyChanged("ViewSelect");
+                OnPropertyChanged("ViewSelect2");
             }
         }
         #endregion
-        #region Properties
+        #region Binding properties
+        #region Title
         string _Title = "Загрузка данных";
         public string Title
         {
@@ -80,46 +107,115 @@ namespace CurrencyConverter.ViewModels
                 OnPropertyChanged("Title");
             }
         }
-        #region Converter
-
-        public string Money1 { 
-            get => converter?.Money1.ToString();
-            set 
-            {
-                if(Decimal.TryParse(value, out decimal result))
-                    converter.Money1 = result;
-                else
-                    converter.Money1 = 0M;
-                OnPropertyChanged("Money1");
-                OnPropertyChanged("Money2");
-            } 
-        }
-        public string Money2
+        #endregion
+        #region List Valutes
+        private ObservableCollection<Valute> _Valutes;
+        public ObservableCollection<Valute> Valutes
         {
-            get => converter?.Money2.ToString();
+            get => _Valutes;
+            private set
+            {
+                _Valutes = value;
+                OnPropertyChanged("Valutes");
+            }
+        }
+        #endregion
+        #region SelectedValute1
+        private Valute _SelectedValute1;
+        public Valute SelectedValute1
+        {
+            get => _SelectedValute1;
+            set
+            {
+                _SelectedValute1 = value;
+                _Converter.Valute1 = value;
+                ViewMain = Visibility.Visible;
+                OnPropertyChanged("SelectedValute1");
+                UpdateFieldsOfConverter();
+            }
+        }
+        #endregion
+        #region SelectedValue2
+        private Valute _SelectedValute2;
+        public Valute SelectedValute2
+        {
+            get => _SelectedValute2;
+            set
+            {
+                _SelectedValute2 = value;
+                _Converter.Valute2 = value;
+                ViewMain = Visibility.Visible;
+                OnPropertyChanged("SelectedValute2");
+                UpdateFieldsOfConverter();
+            }
+        }
+        #endregion
+        #region Money1
+        public string Money1
+        {
+            get => _Converter?.Money1.ToString();
             set
             {
                 if (Decimal.TryParse(value, out decimal result))
-                    converter.Money2 = result;
+                    _Converter.Money1 = result;
                 else
-                    converter.Money2 = 0M;
+                    _Converter.Money1 = 0M;
                 OnPropertyChanged("Money1");
                 OnPropertyChanged("Money2");
             }
         }
         #endregion
+        #region Money2
+        public string Money2
+        {
+            get => _Converter?.Money2.ToString();
+            set
+            {
+                if (Decimal.TryParse(value, out decimal result))
+                    _Converter.Money2 = result;
+                else
+                    _Converter.Money2 = 0M;
+                OnPropertyChanged("Money1");
+                OnPropertyChanged("Money2");
+            }
+        }
+        #endregion
+        #region Valute1
+        private string _Valute1;
+        public string Valute1
+        {
+            get => _Converter?.Valute1.CharCode;
+            private set
+            {
+                _Valute1 = value;
+                OnPropertyChanged("Valute1");
+            }
+        }
+        #endregion
+        #region Valute2
+        public string Valute2
+        {
+            get => _Converter?.Valute2.CharCode;
+            private set
+            {
+
+            }
+        }
+        #endregion
         #endregion
         #region Commands
-        public ICommand LoadedCommand { get; set; }
-        private async void Loaded()
+        #region Loading Data
+        public ICommand LoadingDataCommand { get; set; }
+        private async void LoadingData()
         {
             bool operation = true;
 
             await Task.Run(() => {
                 try
                 {
-                    valutes = new RepositoryValute(new ValutesBuilder()).GetValutes();
-                    converter = new Converter(valutes["USD"], valutes["EUR"]);
+                    var list = new RepositoryValute(new ValutesBuilder()).GetValutes();
+                    _Valutes = new ObservableCollection<Valute>(list.Values);
+                    _Converter = new Converter(_Valutes[0], _Valutes[1]);
                 }
                 catch (Exception)
                 {
@@ -130,6 +226,10 @@ namespace CurrencyConverter.ViewModels
             if (operation)
             {
                 ViewMain = Visibility.Visible;
+                UpdateFieldsOfConverter();
+                SelectedValute1 = _Converter.Valute1;
+                SelectedValute2 = _Converter.Valute2;
+                OnPropertyChanged("Valutes");
             }
             else
             {
@@ -137,9 +237,35 @@ namespace CurrencyConverter.ViewModels
             }
         }
         #endregion
+        #region Show view of valute1 selection
+        public ICommand ShowViewOfValute1SelectionCommand { get; set; }
+        private void Valute1Selection()
+        {
+            ViewSelect1 = Visibility.Visible;
+        }
+        #endregion
+        #region Show view of valute2 selection
+        public ICommand ShowViewOfValute2SelectionCommand { get; set; }
+        private void Valute2Selection()
+        {
+            ViewSelect2 = Visibility.Visible;
+        }
+        #endregion
+        #endregion
+
         public MainPageViewModel()
         {
-            LoadedCommand = new RelayCommand(Loaded);
+            LoadingDataCommand = new RelayCommand(LoadingData);
+            ShowViewOfValute1SelectionCommand = new RelayCommand(Valute1Selection);
+            ShowViewOfValute2SelectionCommand = new RelayCommand(Valute2Selection);
+        }
+
+        private void UpdateFieldsOfConverter()
+        {
+            OnPropertyChanged("Money1");
+            OnPropertyChanged("Money2");
+            OnPropertyChanged("Valute1");
+            OnPropertyChanged("Valute2");
         }
     }
 }
